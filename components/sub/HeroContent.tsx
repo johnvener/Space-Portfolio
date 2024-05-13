0137271c032e3ffc47a3a3e1f74737392dcd92f9
johnvener <johnvenercruz@gmail.com>
2024-05-13 14:50:24 +0800

    Aposthrope change in HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -18,17 +18,17 @@ const HeroContent = () => {
         className ="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
         >
             <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-            <h1 className='Welcome-text text-[13px]'>Fullstack Developer Portfolio</h1>
+            <h1 className="Welcome-text text-[13px]">Fullstack Developer Portfolio</h1>
         </motion.div>
 
         <motion.div
         variants={slideInFromLeft(0.5)}
-        className='flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[900px] w-auto h-auto'
+        className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[900px] w-auto h-auto"
         >
             <span> 
             {/* Providing  */}
             Hi,
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> I'm Vener </span>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> I&apos;m Vener </span>
             {/* a Full Stack Software Engineer */}
             <br/>
             Nice to finally meet you.
